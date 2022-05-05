d7c0537598592481ebc1a8b1cfb5bbeb83f7919d
Raphael <raphaelfreire.dias@gmail.com>
2022-05-05 09:06:59 -0300

    style: :art: Ajustando estrutura de pastas, e separação de server e app

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
-import { NodemailerMailAdapter } from "./adapters/nodemailer/nodemailer-mail-adapter";
-import { PrismaFeedbacksRepository } from "./repositories/prisma/prisma-feedback-repository";
-import { SubmitFeedbackUseCase } from "./use-cases/submit-feedback-use-case";
+import { NodemailerMailAdapter } from "../adapters/nodemailer/nodemailer-mail-adapter";
+import { PrismaFeedbacksRepository } from "../repositories/prisma/prisma-feedback-repository";
+import { SubmitFeedbackUseCase } from "../use-cases/submit-feedback-use-case";
 export const routes = express.Router();
 
 routes.post("/feedbacks", async (req, res) => {
